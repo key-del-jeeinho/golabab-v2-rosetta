620592fec5316a47515cff51435ed33e4657b367
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-12-01 11:46:32 +0900

    [ADD] 누락된 javadoc 추가

--- src/main/java/io/github/key_del_jeeinho/golabab_v2/rosetta/lunch/DayMenuDto.java
+++ src/main/java/io/github/key_del_jeeinho/golabab_v2/rosetta/lunch/DayMenuDto.java
@@ -1,12 +1,18 @@
 package io.github.key_del_jeeinho.golabab_v2.rosetta.lunch;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
 
 import java.util.List;
 import java.util.Map;
 
-@Builder
+/**
+ * 일일 식단 메뉴 정보를 담고있는 DTO 입니다.
+ * 급식 조회에 사용됩니다.
+ *
+ * @author 지인호
+ * @since 1.0
+ */
+
 public record DayMenuDto (
         @JsonProperty("id") long id,
         @JsonProperty("menu") Map<MealType, List<String>> menu //하루동안 나올 식단 메뉴
--- src/main/java/io/github/key_del_jeeinho/golabab_v2/rosetta/lunch/MealMenuDto.java
+++ src/main/java/io/github/key_del_jeeinho/golabab_v2/rosetta/lunch/MealMenuDto.java
@@ -1,11 +1,17 @@
 package io.github.key_del_jeeinho.golabab_v2.rosetta.lunch;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
 
 import java.util.List;
 
-@Builder
+/**
+ * 조/중/석식 식단 메뉴 정보를 담고있는 DTO 입니다.
+ * 급식 조회에 사용됩니다.
+ *
+ * @author 지인호
+ * @since 1.0
+ */
+
 public record MealMenuDto (
         @JsonProperty("id") long id,
         @JsonProperty("meal") MealType mealType, //해당 식단이 조식인지, 중식인지, 석식인지를 명시한다
